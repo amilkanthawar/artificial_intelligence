adf30519205231a30160f93dd36544e6475d24cc
Vivek Amilkanthawar <mailvivek.s.a@gmail.com>
2017-09-06 23:49:03 +0530

    Initial Solution. Need Improvement

--- BotSavesPrincess/src/Solution.java
+++ BotSavesPrincess/src/Solution.java
@@ -2,10 +2,41 @@ import java.util.Scanner;
 
 public class Solution {
 	static void displayPathtoPrincess(int n, String[] grid) {
-		findPrincessLocation(n, grid);
+		int princessLocation = findPrincessLocation(n, grid);
+
+		if (princessLocation <= 2) {
+			for (int i = 0; i < n / 2; i++) {
+				System.out.println("LEFT");
+			}
+		} else {
+			for (int i = 0; i < n / 2; i++) {
+				System.out.println("RIGHT");
+			}
+		}
+
+		if (princessLocation % 2 == 0) {
+			for (int i = 0; i < n / 2; i++) {
+				System.out.println("DOWN");
+			}
+		} else {
+			for (int i = 0; i < n / 2; i++) {
+				System.out.println("UP");
+			}
+		}
+
 	}
 
 	static int findPrincessLocation(int m, String[] grid) {
+
+		if ("p".equals(grid[0].substring(0, 1))) {
+			return 1;
+		} else if ("p".equals(grid[m - 1].substring(0, 1))) {
+			return 2;
+		} else if ("p".equals(grid[0].substring(m - 1, m))) {
+			return 3;
+		} else if ("p".equals(grid[m - 1].substring(m - 1, m))) {
+			return 4;
+		}
 		return 0;
 	}
 
